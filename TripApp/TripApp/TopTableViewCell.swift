8967476b9a845004171d27844ccf7fefab65ba4a
nakajimase <seiya.nakajima.jpn@gmail.com>
2018-07-14 19:44:29 +0900

    Modify TOP画面のデータ取得・表示

---
+++ TripApp/TripApp/TopTableViewCell.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class TopTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    @IBOutlet weak var topImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TripApp/TripApp/TopViewController.swift
@@ -0,0 +1,73 @@
+import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
+
+class TopViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var listTableView: UITableView!
+    
+    private var articlesList: [[String: String?]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.listTableView.delegate = self
+        self.listTableView.dataSource = self
+        
+        self.listTableView.register(UINib(nibName: "TopTableViewCell", bundle: nil), forCellReuseIdentifier: "TopTableViewCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.articlesList.count == 0 {
+            return 0
+        } else {
+            return self.articlesList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopTableViewCell") as! TopTableViewCell
+        let article = articlesList[indexPath.row]
+        cell.titleLabel?.text = article["title"] as? String
+        cell.subLabel?.text = article["area"] as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        getData()
+        let storyboard: UIStoryboard = UIStoryboard(name: "DetailViewController", bundle: nil)
+        let nextView = storyboard.instantiateInitialViewController() as! DetailViewController
+        self.present(nextView, animated: true, completion: nil)
+    }
+    
+    func getData() {
+        Alamofire.request("http://13.59.253.231/article/getList").responseJSON{ response in
+            guard let object = response.result.value else {return}
+            let listJsonData = JSON(object)
+//            print(listJsonData)
+            listJsonData.forEach {(_, listJsonData) in
+                let article: [String: String?] = [
+                    "title": listJsonData["article_title"].string,
+                    "area": listJsonData["area"].string,
+                    "category": listJsonData["category"].string
+                ]
+                self.articlesList.append(article)
+            }
+            self.listTableView.reloadData()
+            SVProgressHUD.dismiss()
+        }
+        SVProgressHUD.show()
+    }
+}
+
